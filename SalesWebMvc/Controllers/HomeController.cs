e2a5ab37ef9ce037b747219ac797b476f3aab3e8
Bruno Santos <boasortebruno@gmail.com>
2020-04-09 15:35:58 -0300

    First controller and Razer pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Student"] = "Bruno S Santos";
 
             return View();
         }
@@ -25,7 +26,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
+            
             return View();
         }
 
